b8655535fefb58eb93adbfa07da7392a852a8828
Sam Clamons (Illumina) <sclamons@illumina.com>
2021-11-15 11:57:24 -0500

    Adds manual BASH-based sanity tests in a 'tests' folder.

---
+++ tests/process_sanity_test_outputs.py
@@ -0,0 +1,52 @@
+import os
+import sys
+
+def main():
+    test_output_dir = sys.argv[1]
+    frozen_output_dir = sys.argv[2]
+
+    with open('sanity_test_outputs.html', 'w') as html_file:
+        html_file.write(
+            '''<!DOCTYPE html>
+<html>
+  <head>
+    <title>Sanity test outputs</title>
+  </head>
+  <style>
+  * {
+  box-sizing: border-box;
+  }
+  img{
+  max-width: 100%;
+  }
+  .column {
+    float: left;
+    width: 50%;
+    padding: 10px;
+  }
+  </style>
+  <body>''')
+
+        for image_filename in os.listdir(test_output_dir):
+            sample_id = image_filename.split(".")[0]
+            if not os.path.exists(os.path.join(frozen_output_dir, image_filename)):
+                raise ValueError(f"No matching frozen result for test output {image_filename}")
+            html_file.write('<div style="border: 10x solid red;">')
+
+            # New result
+            html_file.write('<div class="column">')
+            html_file.write(f"<h2>New {sample_id}</h2>")
+            html_file.write(f'\n<img src="{os.path.join(test_output_dir, image_filename)}"/>')
+            html_file.write('</div>')
+
+            # Frozen result
+            html_file.write('<div class="column">')
+            html_file.write(f"<h2>Frozen {sample_id}</h2>")
+            html_file.write(f'\n<img src="{os.path.join(frozen_output_dir, image_filename)}"/>')
+            html_file.write("</div>")
+
+            html_file.write("<div>")
+        html_file.write("</body>")
+        html_file.write("</html>")
+if __name__ == "__main__":
+    main()

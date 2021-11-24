d7a3b11e651ed572fe11edc9e853d7ff8e2b1cd9
egor-dolzhenko <egor.dolzhenko@gmail.com>
2021-11-24 10:38:06 -0800

    Implement base counting

--- reviewer/snps/Workflow.cpp
+++ reviewer/snps/Workflow.cpp
@@ -24,9 +24,18 @@
 #include <stdexcept>
 #include <vector>
 
+#include "graphalign/GraphAlignmentOperations.hh"
+#include "graphalign/LinearAlignmentOperations.hh"
+
+using graphtools::Alignment;
+using graphtools::getQuerySequencesForEachNode;
+using graphtools::getSequencesForEachOperation;
 using graphtools::Operation;
 using graphtools::OperationType;
+using std::string;
 using std::vector;
+
+using AlignedRead = std::pair<std::string, ReadPathAlign>;
 
 namespace snps
 {
@@ -64,49 +73,60 @@ struct BaseCounts
     std::array<int, 4> counts;
 };
 
-vector<BaseCounts> getBaseCountVector(const GraphPath& path, const vector<const ReadPathAlign*>& alignPtrs)
+void updateBaseCounts(
+    const string& bases, const string& nodeSeq, const Alignment& align, int startPos, vector<BaseCounts>& baseCountVec)
+{
+    auto seqsByOperation = getSequencesForEachOperation(align, nodeSeq, bases);
+    auto seqsByOperationIt = seqsByOperation.begin();
+
+    for (const Operation& operation : align)
+    {
+        const auto& operationReferenceSeq = seqsByOperationIt->first;
+        const auto& operationBaseSeq = seqsByOperationIt->second;
+        switch (operation.type())
+        {
+        case OperationType::kMatch:
+        case OperationType::kMismatch:
+            for (int offset = 0; offset != operation.referenceLength(); ++offset)
+            {
+                auto base = operationBaseSeq[offset];
+                baseCountVec[startPos + offset].counts[getBaseIndex(base)] += 1;
+            }
+            break;
+        default:
+            break;
+        }
+
+        ++seqsByOperationIt;
+        startPos += static_cast<int>(operation.referenceLength());
+    }
+}
+
+vector<BaseCounts> getBaseCountVector(const GraphPath& path, const vector<AlignedRead>& alignedReads)
 {
     vector<BaseCounts> baseCountVec;
     baseCountVec.resize(path.length());
 
-    for (const auto& pathAlignPtr : alignPtrs)
+    for (const auto& alignedRead : alignedReads)
     {
-        const auto& pathAlign = *pathAlignPtr;
-        // std::cerr << "\t" << *pathAlign.align << " (" << pathAlign.begin << "-" << pathAlign.end << ")" << std::endl;
+        const auto& bases = alignedRead.first;
+        const auto& pathAlign = alignedRead.second;
+        const int numNodesInAlign = static_cast<int>(pathAlign.align->path().numNodes());
+        auto readPiecesByNode = getQuerySequencesForEachNode(*pathAlign.align, bases);
+        auto nodeReadPieceIt = readPiecesByNode.begin();
 
         int posOnPath = pathAlign.begin;
-        for (const auto& nodeAlign : pathAlign.align->alignments())
-        {
-            for (const Operation& operation : nodeAlign)
-            {
-                switch (operation.type())
-                {
-                case OperationType::kMatch:
-                    for (int pos = posOnPath; pos != posOnPath + operation.referenceLength(); ++pos)
-                    {
-                        baseCountVec[pos].counts[getBaseIndex('A')] += 1;
-                    }
-                    break;
-                /*
-                case OperationType::kMismatch:
-                    score += mismatchScore * operation.referenceLength();
-                    break;
-                case OperationType::kInsertionToRef:
-                    score += gapScore * operation.queryLength();
-                    break;
-                case OperationType::kDeletionFromRef:
-                    score += gapScore * operation.referenceLength();
-                    break; */
-                default:
-                    break;
-                }
-            }
 
-            // auto startNodeIndex = pathAlign.startIndexOnPath;
-            // auto endNodeIndex = startNodeIndex + pathAlign.align->path().numNodes();
-            // for (int nodeIndex = startNodeIndex; nodeIndex != endNodeIndex) {
-            //
-            // }
+        for (int nodeIndex = 0; nodeIndex != numNodesInAlign; ++nodeIndex)
+        {
+            const auto node = pathAlign.align->path().getNodeIdByIndex(nodeIndex);
+            const auto& nodeSeq = pathAlign.align->path().graphRawPtr()->nodeSeq(node);
+            const auto& nodeAlign = pathAlign.align->alignments()[nodeIndex];
+
+            updateBaseCounts(*nodeReadPieceIt, nodeSeq, nodeAlign, posOnPath, baseCountVec);
+
+            ++nodeReadPieceIt;
+            posOnPath += static_cast<int>(pathAlign.align->referenceLength());
         }
     }
     return baseCountVec;
@@ -118,7 +138,7 @@ void callSnps(
 {
     for (int hapIndex = 0; hapIndex != paths.size(); ++hapIndex)
     {
-        vector<const ReadPathAlign*> hapAligns;
+        vector<AlignedRead> haplotypeReads;
         for (int fragIndex = 0; fragIndex != fragAssignment.fragIds.size(); ++fragIndex)
         {
             const auto& fragId = fragAssignment.fragIds[fragIndex];
@@ -128,19 +148,26 @@ void callSnps(
 
             if (fragPathAlign.readAlign.pathIndex == hapIndex)
             {
-                hapAligns.push_back(&fragPathAlign.readAlign);
-                hapAligns.push_back(&fragPathAlign.mateAlign);
+                const auto& readBases = fragById.at(fragId).read.bases;
+                const auto& mateBases = fragById.at(fragId).mate.bases;
+                haplotypeReads.emplace_back(std::make_pair(readBases, fragPathAlign.readAlign));
+                haplotypeReads.emplace_back(std::make_pair(mateBases, fragPathAlign.mateAlign));
             }
         }
 
-        auto baseCountVec = getBaseCountVector(paths[hapIndex], hapAligns);
-        for (const auto& baseCounts : baseCountVec)
+        std::cerr << "Hap\tRef\tA\tT\tC\tG" << std::endl;
+        auto baseCountVec = getBaseCountVector(paths[hapIndex], haplotypeReads);
+        const auto& haplotypeSeq = paths[hapIndex].seq();
+
+        for (int index = 0; index != baseCountVec.size(); ++index)
         {
-            auto aCount = baseCounts.getCount('A');
-            auto tCount = baseCounts.getCount('T');
-            auto cCount = baseCounts.getCount('C');
-            auto gCount = baseCounts.getCount('G');
-            std::cerr << hapIndex << "\t" << aCount << "\t" << tCount << "\t" << cCount << "\t" << gCount << std::endl;
+            const auto& baseCounts = baseCountVec[index];
+            char ref = haplotypeSeq[index];
+            auto as = baseCounts.getCount('A');
+            auto ts = baseCounts.getCount('T');
+            auto cs = baseCounts.getCount('C');
+            auto gs = baseCounts.getCount('G');
+            std::cerr << hapIndex << "\t" << ref << "\t" << as << "\t" << ts << "\t" << cs << "\t" << gs << std::endl;
         }
     }
 }
